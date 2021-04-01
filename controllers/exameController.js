04fa3a9f9f9e1a9206f1f10c856f951a5901b041
Juliano A Cassoli <jucassoli@gmail.com>
2021-04-01 14:12:02 -0300

    add missing find exame by nome completing associated labs

--- controllers/exameController.js
+++ controllers/exameController.js
@@ -130,3 +130,4 @@ exports.findById = (req, res) => {
     }
   });
 };
+
--- controllers/labExamsController.js
+++ controllers/labExamsController.js
@@ -1,4 +1,7 @@
 const LabExam = require("../models/labExam");
+const Lab = require("../models/laboratorio");
+const Exame = require("../models/exame");
+
 const common = require("../common/common");
 
 exports.associate = (req, res) => {
@@ -7,17 +10,17 @@ exports.associate = (req, res) => {
   if (Array.isArray(data)) {
     let aditionsArray = [];
     for (let item of data) {
-      aditionsArray.push(createSingle(item, Exame));
+      aditionsArray.push(createSingle(item, LabExam));
     }
     Promise.all(aditionsArray).then(result => {
-      res.status(201).json({ "created": result });
+      res.status(201).json({ "created": common.fixResponseData(result) });
     }).catch(err => {
       res.status(500).send(err);
     });
 
   } else {
-    createSingle(data, Exame).then(result => {
-      res.status(201).json({ "created": result });
+    createSingle(data, LabExam).then(result => {
+      res.status(201).json({ "created": common.fixResponseData(result) });
     }).catch(err => {
       if (common.isValidationError(err)) {
         res.status(400)
@@ -37,9 +40,68 @@ let createSingle = (data, LabExamSchema) => {
       if (err) {
         reject({ isSuccess: false, err });
       } else {
-        resolve({ isSuccess: true, exame: newLabExam });
+        resolve({ isSuccess: true, exame: common.fixResponseData(newLabExam) });
       }
     });
   });
 };
 
+exports.findExamsByNome = (req, res) => {
+
+  let filter = Object.assign({},
+    (req.query.removido) ? { removido: req.query.removido } : { removido: false },
+    (req.query.status) && { status: req.query.status },
+    (req.query.tipo) && { tipo: req.query.tipo },
+    (req.query.nome) && { nome: req.query.nome }
+  );
+
+  let schema = Exame.find(filter);
+
+  if (req.query.limit) {
+    let limit = req.query.limit <= 100 ? parseInt(req.query.limit) : 10;
+    let page = 0;
+
+    if (req.query.page) {
+      let pageNum = parseInt(req.query.page);
+      page = Number.isInteger(pageNum) ? pageNum : 0;
+    }
+
+    schema.limit(limit).skip(limit * page);
+  }
+
+  schema.exec(function (err, examsFound) {
+    if (err) {
+      res.status(500).send(err);
+    }
+    if (Array.isArray(examsFound) && examsFound.length == 0) {
+      res.status(404).send();
+    } else {
+
+      if(Array.isArray(examsFound) && examsFound.length > 0) {
+
+        examsFound.forEach(oneExam => {
+          let exameId = oneExam._id;
+          LabExam.find({ exameId }).exec(labExamsFound => {
+
+            if(Array.isArray(labExamsFound) && labExamsFound.length > 0) {
+
+              let resultLabs = [];
+              labExamsFound.forEach(oneLabExam => {
+                let laboratorioId = oneLabExam.laboratorioId;
+                Lab.findById(laboratorioId).exec(oneLabFound => {
+                  resultLabs.push(common.fixResponseData(oneLabFound));
+                });
+              });
+              Object.assign(common.fixResponseData(oneExam), { laboratorios_associados: resultLabs })
+            }
+          });
+
+        });
+
+      }
+
+      res.json(examsFound);
+    }
+  });
+
+};
